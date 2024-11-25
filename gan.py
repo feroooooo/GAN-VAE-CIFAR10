1905fc0f5d3cf10351fd85af094173115b8aec45
feroooooo <1799363617@qq.com>
2024-11-25 18:50:32 +0800

    dcgan

--- gan.py
+++ gan.py
@@ -77,3 +77,74 @@ class DiscriminatorCIFAR10(nn.Module):
         x = self.fc3(x)
         x = self.fc4(x)
         return x
+    
+
+class GeneratorDCGAN(nn.Module):
+    def __init__(self, nz=100, ngf=64, nc=3):
+       """
+       参数说明：
+       nz: 噪声向量的维度
+       ngf: 生成器特征图的基础通道数
+       nc: 输出图像的通道数(CIFAR10为3)
+       """
+       super(GeneratorDCGAN, self).__init__()
+       
+       self.main = nn.Sequential(
+           # 输入是 nz x 1 x 1
+           nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+           nn.BatchNorm2d(ngf * 8),
+           nn.ReLU(True),
+           # 状态尺寸: (ngf*8) x 4 x 4
+           
+           nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+           nn.BatchNorm2d(ngf * 4),
+           nn.ReLU(True),
+           # 状态尺寸: (ngf*4) x 8 x 8
+           
+           nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+           nn.BatchNorm2d(ngf * 2),
+           nn.ReLU(True),
+           # 状态尺寸: (ngf*2) x 16 x 16
+           
+           nn.ConvTranspose2d(ngf * 2, nc, 4, 2, 1, bias=False),
+           nn.Tanh()
+           # 最终尺寸: nc x 32 x 32
+       )
+    
+    def forward(self, x):
+       # 输入向量需要重塑为 (batch_size, nz, 1, 1)
+       x = x.view(x.size(0), -1, 1, 1)
+       return self.main(x)
+   
+
+class DiscriminatorDCGAN(nn.Module):
+    def __init__(self, nc=3, ndf=64):
+       """
+       参数说明：
+       nc: 输入图像的通道数
+       ndf: 判别器特征图的基础通道数
+       """
+       super(DiscriminatorDCGAN, self).__init__()
+       
+       self.main = nn.Sequential(
+           # 输入是 nc x 32 x 32
+           nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+           nn.LeakyReLU(0.2, inplace=True),
+           # 状态尺寸: ndf x 16 x 16
+           
+           nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+           nn.BatchNorm2d(ndf * 2),
+           nn.LeakyReLU(0.2, inplace=True),
+           # 状态尺寸: (ndf*2) x 8 x 8
+           
+           nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+           nn.BatchNorm2d(ndf * 4),
+           nn.LeakyReLU(0.2, inplace=True),
+           # 状态尺寸: (ndf*4) x 4 x 4
+           
+           nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
+           nn.Sigmoid()
+           # 输出尺寸: 1 x 1 x 1
+       )
+    def forward(self, x):
+       return self.main(x).view(-1, 1).squeeze(1)
